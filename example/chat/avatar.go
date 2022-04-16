bfe885b5a9687edec8a9fe369c03dfcc43e253f2
YoongdooNoh <yoongdoo0819@naver.com>
2022-04-16 16:21:35 +0900

    GetAvatarURL func defined

---
+++ example/chat/avatar.go
@@ -0,0 +1,24 @@
+package chat
+
+import (
+	"errors"
+)
+
+var ErrNoAvatarURL = errors.New("chat: Unable to get an avatar URL.")
+
+type Avatar interface {
+	GetAvatarURL(c *client) (string, error)
+}
+
+type AuthAvatar struct{}
+
+var UseAuthAvatar AuthAvatar
+
+func (AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
